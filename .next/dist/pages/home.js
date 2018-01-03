5027aef648a3a59db1ba639656c14d80dd324543
Justin Rhoads <rhoadsjustin@gmail.com>
2018-01-03 13:03:04 -0600

    added project links, styled the project page slightly, and started added message links

---
+++ .next/dist/pages/home.js
@@ -0,0 +1,42 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _MyLayout = require('../components/MyLayout');
+
+var _MyLayout2 = _interopRequireDefault(_MyLayout);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/justinrhoads/wdi/next-portfolio/pages/home.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement(_MyLayout2.default, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 6
+        }
+    }, 'Welcome'), _react2.default.createElement('div', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 9
+        }
+    }, _react2.default.createElement('p', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 10
+        }
+    }, 'I am a passionate and skilled web developer. My passion for creating is driven by a desire to use technology to better the world around me. Simple, intuitive, and engaging designs are the solutions I always look for when problems are brought before me. I bring leadership, collaborative, and public-speaking skills from my previous career to complement my strong development skills.')));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2hvbWUuanMiXSwibmFtZXMiOlsiTGF5b3V0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsQUFBTyxBQUFZLEFBR25COzs7Ozs7OztrQkFBZSxZQUFBOzJCQUNYLEFBQUM7O3NCQUFEO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsa0JBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLE9BREosQUFDSSxBQUdBLDRCQUFBLGNBQUE7O3NCQUFBO3dCQUFBLEFBQ0k7QUFESjtBQUFBLHVCQUNJLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxPQU5HLEFBQ1gsQUFJSSxBQUNJO0FBTloiLCJmaWxlIjoiaG9tZS5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvanVzdGlucmhvYWRzL3dkaS9uZXh0LXBvcnRmb2xpbyJ9
--- pages/skills.js
+++ pages/skills.js
@@ -13,7 +13,10 @@ export default () => (
             <li>Javascript</li>
             <li>Public Speaking
                 <div>
-                    Here are some links to videos:
+                    Here are some links to my messages:
+                        <ul>
+                        <li>"Raised to Life" <a href="https://crossroadschurchaustin.com/messages/2017/11/30/raised-to-life">click here to listen</a></li>
+                        </ul>
                 </div>
             </li>
         </ul>
